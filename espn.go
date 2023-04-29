0264fed8ed58e2b2ce75749765a8db7234c5cf15
Brady Dowling <brady@Bradys-MacBook-Pro.local>
2023-04-29 19:36:05 -0400

    Add initial go files as told to by chatgpt

--- espn.go
+++ espn.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	// Make HTTP GET request to ESPN.com
+	res, err := http.Get("https://www.espn.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	// Parse the HTML using goquery
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Find all the frontpage headlines
+	headlines := doc.Find(".col-three .headlineStack ul li")
+
+	// Print out the headlines
+	fmt.Println("ESPN Frontpage Headlines:")
+	headlines.Each(func(i int, s *goquery.Selection) {
+		fmt.Printf("%d. %s\n", i+1, s.Text())
+	})
 }
